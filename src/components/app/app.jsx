f683fe2c1320ea75eaac0268398e3e8801431ef9
Yulia Karpova <karpova.jul@gmail.com>
2020-06-19 16:30:42 +1000

    3.4 Подключит react-router-dom и настроит роутинг

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
 
 const welcomeButtonHandler = () => {};
 
@@ -8,10 +11,22 @@ const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen
-      errorsCount={errorsCount}
-      onWelcomeButtonClick={welcomeButtonHandler}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorsCount={errorsCount}
+            onWelcomeButtonClick={welcomeButtonHandler}
+          />
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen/>
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionScreen/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
