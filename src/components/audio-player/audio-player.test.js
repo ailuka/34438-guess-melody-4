eaba9ba9d78f9f1df0247d1128f3ada5b9cac456
Yulia Karpova <karpova.jul@gmail.com>
2020-06-27 18:32:22 +1000

    4.4 Создаст снепшот-тест для компонента AudioPlayer, обновит снепшоты компонентов Artist и Genre

---
+++ src/components/audio-player/audio-player.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import AudioPlayer from "./audio-player.jsx";
+
+const mock = {
+  song: {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+  },
+};
+
+describe(`src/audio-player.jsx`, () => {
+  describe(`when there is a song`, () => {
+    it(`should render`, () => {
+      const {song} = mock;
+
+      const tree = renderer.create(
+          <AudioPlayer
+            isPlaying={false}
+            src={song.src}
+          />
+      )
+      .toJSON();
+
+      expect(tree).toMatchSnapshot();
+    });
+  });
+});
