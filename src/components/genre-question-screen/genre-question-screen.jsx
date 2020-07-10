cca9e8ba628264d3bc94bc997b8ea6fab6b05183
Yulia Karpova <karpova.jul@gmail.com>
2020-07-10 13:12:01 +1000

    6.5 Вынесет вопрос по жанру в отдельный компонент

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {GameType} from "../../const.js";
+import GenreQuestionItem from "../genre-question-item/genre-question-item.jsx";
 
 class GenreQuestionScreen extends PureComponent {
   render() {
@@ -18,21 +19,14 @@ class GenreQuestionScreen extends PureComponent {
           }}
         >
           {answers.map((answer, i) => (
-            <div key={`${i}-${answer.src}`} className="track">
-              {renderPlayer(answer.src, i)}
-              <div className="game__answer">
-                <input
-                  className="game__input visually-hidden"
-                  type="checkbox"
-                  name="answer"
-                  value={`answer-${i}`}
-                  id={`answer-${i}`}
-                  checked={userAnswers[i]}
-                  onChange={onChange}
-                />
-                <label className="game__check" htmlFor={`answer-${i}`}>Отметить</label>
-              </div>
-            </div>
+            <GenreQuestionItem
+              answer={answer}
+              id={i}
+              key={`${i}-${answer.src}`}
+              onChange={onChange}
+              renderPlayer={renderPlayer}
+              userAnswer={userAnswers[i]}
+            />
           ))}
 
           <button className="game__submit button" type="submit">Ответить</button>
