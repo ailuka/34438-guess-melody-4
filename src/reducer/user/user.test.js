159123d8d963e5c9416d229a08e3e07bd51f03e9
Yulia Karpova <karpova.jul@gmail.com>
2020-07-21 10:44:06 +1000

    7.8 Добавит файл-редьюсер user.js с действием, которое будет отвечать за обновление поля authorizationStatus

---
+++ src/reducer/user/user.test.js
@@ -0,0 +1,61 @@
+import {AuthorizationStatus, ActionType, ActionCreator, reducer} from "./user.js";
+
+describe(`Action creators work correctly`, () => {
+  it(`Action creator for require authorization returns correct action`, () => {
+    expect(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH)).toEqual({
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.NO_AUTH,
+    });
+
+    expect(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)).toEqual({
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    });
+  });
+});
+
+describe(`Reducer works correctly`, () => {
+  it(`without addition parameters returns initial state`, () => {
+    expect(reducer(undefined, {})).toEqual({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    });
+  });
+
+  it(`Reducer changes authorizationStatus for a given value`, () => {
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    });
+
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.NO_AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    });
+
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    });
+
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.NO_AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    });
+  });
+});
