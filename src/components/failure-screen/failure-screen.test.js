2a037a958f407f9ff3533fed7b57915c62ceadc1
Yulia Karpova <karpova.jul@gmail.com>
2020-07-27 18:20:46 +1000

    8.11 Обновит тесты

--- src/components/failure-screen/failure-screen.test.js
+++ src/components/failure-screen/failure-screen.test.js
@@ -1,13 +1,17 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import FailureScreen from "./failure-screen.jsx";
+import {Router} from "react-router-dom";
+import history from "../../history.js";
 
 describe(`FailureScreen`, () => {
   it(`should render correctly`, () => {
     const failureScreen = renderer.create(
-        <FailureScreen
-          onReplayButtonClick={() => null}
-        />
+        <Router history={history}>
+          <FailureScreen
+            onReplayButtonClick={() => null}
+          />
+        </Router>
     ).toJSON();
 
     expect(failureScreen).toMatchSnapshot();
